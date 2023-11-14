3ea69f60b98f37d342a9a0d48a40e952eaaa2515
JonasEmre <y.emretoktas@gmail.com>
2023-11-14 03:29:17 +0300

    v0.002

--- synth_boutique/urls.py
+++ synth_boutique/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('item/', include('items.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
